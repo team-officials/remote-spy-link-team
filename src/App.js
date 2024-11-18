da1ec43bfd3efd03a36d452f61faa30d3937f2c6
FlowPc <emmykulforsure@gmail.com>
2024-11-18 02:11:03 -0800

    new-errow-connect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
 import PayymentGate from "./Components/PayymentGate";
 import Location from "./Components/Location";
@@ -8,24 +8,31 @@ import RemoteLink from "./Components/Remotelink/RemoteLink";
 import Navbar from "./Navbar/Navbar";
 import SecondPage from "./pages/SecondPage";
 
-
-
 function App() {
-
-  
   return (
     <BrowserRouter>
-    <Navbar />
+      <NavBarWrapper />
       <Routes>
-        <Route path="/pay" element= { <PayymentGate />} />
-        <Route path="/location" element= { <Location /> } />
-        <Route path="/memberCard" element= { <MembersshipCard /> } />
+        <Route path="/pay" element={<PayymentGate />} />
+        <Route path="/location" element={<Location />} />
+        <Route path="/memberCard" element={<MembersshipCard />} />
         <Route path="/access" element={<AccessCode />} />
         <Route path="/" element={<RemoteLink />} />
         <Route path="/secondpage" element={<SecondPage />} />
       </Routes>
     </BrowserRouter>
+  );
+}
 
+// A new component to handle the conditional rendering of Navbar
+function NavBarWrapper() {
+  const location = useLocation(); // Get the current location
+
+  return (
+    <>
+      {/* Conditionally render Navbar only on routes other than '/' */}
+      {location.pathname !== "/" && <Navbar />}
+    </>
   );
 }
 
